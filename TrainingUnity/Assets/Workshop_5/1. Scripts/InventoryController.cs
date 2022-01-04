308f2a8be02ea595fccfea6989ac1b48160a6e33
nanggiomientrung <miracle.t084@gmail.com>
2022-01-04 21:42:51 +0700

    workshop 5: inventory board

--- TrainingUnity/Assets/Workshop_5/1. Scripts/InventoryController.cs
+++ TrainingUnity/Assets/Workshop_5/1. Scripts/InventoryController.cs
@@ -1,14 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryController : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private Transform content;
+    [SerializeField] private InventoryItemScript prefab;
+    [SerializeField] private Text itemInformationText;
+
+    public static InventoryController instance;
+    private void Awake()
     {
-        
+        instance = this;
     }
 
+    public void SelectItem(int itemIndex)
+    {
+        itemInformationText.text = string.Format($"{items.ItemList[itemIndex].ItemName}\n{items.ItemList[itemIndex].ItemType}");
+    }
 
+    private InventoryItemScript currentSpawnedItem;
+    void Start()
+    {
+        RetrieveData();
+        for (int i = 0; i < items.ItemList.Count; i++)
+        {
+            currentSpawnedItem = Instantiate(prefab, content);
 
+            currentSpawnedItem.SetDataToItem(i, items.ItemList[i].ItemName, items.ItemList[i].ItemType, items.ItemList[i].ItemData);
+        }
+    }
+
+    #region LOAD ITEM FROM LOCAL
+    InventoryItemList items = new InventoryItemList();
+    private void RetrieveData()
+    {
+        string dataString = ReadDataFromPersistent("InventoryData");
+        if (dataString != "")
+        {
+            items = JsonUtility.FromJson<InventoryItemList>(dataString);
+        }
+        else
+        {
+            items = new InventoryItemList();
+        }
+    }
+
+    private string ReadDataFromPersistent(string fileName)
+    {
+        string dataPath = $"{Application.persistentDataPath}/{fileName}.txt";
+        if (System.IO.File.Exists(dataPath))
+        {
+            return System.IO.File.ReadAllText(dataPath);
+        }
+        else
+        {
+            return "";
+        }
+    }
+    #endregion LOAD ITEM FROM LOCAL
 }
---
+++ TrainingUnity/Assets/Workshop_5/1. Scripts/InventoryItemScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryItemScript : MonoBehaviour
+{
+    [SerializeField] private Image iconImage;
+    [SerializeField] private Button selfButton;
+    private string itemName; // tên của item
+    private InventoryItemType itemType;
+    private string itemData; // chính là json lưu data của item
+
+    private int itemIndex;
+
+    private void Start()
+    {
+        selfButton.onClick.AddListener(SelectItem);
+    }
+
+    public void SetDataToItem(int ItemIndex, string ItemName, InventoryItemType ItemType, string ItemData)
+    {
+        itemIndex = ItemIndex;
+        itemName = ItemName;
+        itemData = ItemData;
+        itemType = ItemType;
+        switch (ItemType)
+        {
+            case InventoryItemType.LifeFlask:
+                iconImage.sprite = Resources.Load<Sprite>("InventoryIcon/LifeFlask");
+                break;
+            case InventoryItemType.ManaFlask:
+                iconImage.sprite = Resources.Load<Sprite>("InventoryIcon/ManaFlask");
+                break;
+            case InventoryItemType.Food:
+                iconImage.sprite = Resources.Load<Sprite>("InventoryIcon/Food");
+                break;
+            case InventoryItemType.Dagger:
+            case InventoryItemType.Sword:
+                iconImage.sprite = Resources.Load<Sprite>("InventoryIcon/Weapon");
+                break;
+            case InventoryItemType.BodyArmour:
+            case InventoryItemType.Gloves:
+                iconImage.sprite = Resources.Load<Sprite>("InventoryIcon/Armour");
+                break;
+        }
+    }
+
+    private void SelectItem()
+    {
+        InventoryController.instance.SelectItem(itemIndex);
+    }
+}
